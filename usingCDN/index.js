4cf271e8ed125640c494f897d6d3b3fffe7bf3d4
Harshit Agarwal <56046211+harshit7388@users.noreply.github.com>
2023-01-21 15:05:51 +0530

    Create index.js

---
+++ usingCDN/index.js
@@ -0,0 +1,17 @@
+// ReactDOM.render(<h1> hello yay yay !!</h1>, document.getElementById("root"));
+
+
+const navbar = (
+
+    <nav>
+
+        <h1>website name</h1>
+        <ul>
+            <li>menu</li>
+            <li>about</li>
+            <li>contact</li>
+
+        </ul>
+    </nav>
+)
+ReactDOM.render(navbar, document.getElementById("root"));
