fd42675c22a6ca57e3ddfdd4fbc4473c7375e1eb
Harshit Agarwal <56046211+harshit7388@users.noreply.github.com>
2023-01-24 15:32:34 +0530

    Add files via upload

---
+++ react_pro_1/components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Main(){
+    return (
+        <main>
+                <h1 className = "main--title" >React facts</h1>
+                <ul className  ="main--facts" >
+                    <li>choice 1</li>
+                    <li>choice 2</li>
+                    <li>choice 3</li>
+                </ul>
+        </main>
+    )
+}
