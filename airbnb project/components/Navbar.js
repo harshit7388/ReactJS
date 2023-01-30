b9cc55edb0c17494616626b7d58d76cebd38c231
Harshit Agarwal <56046211+harshit7388@users.noreply.github.com>
2023-01-30 11:34:33 +0530

    Delete airbnb project directory

--- airbnb project/components/Navbar.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function Navbar() {
-    return (
-        <nav>
-            <img src="../images/download.jpg" className="nav--logo"/>
-        </nav>
-    )
-}
